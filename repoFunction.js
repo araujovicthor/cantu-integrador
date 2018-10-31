010873856a779c2e3c4fba4f765fdeaa47da573a
Victhor Araújo <victhor.araujo.41@gmail.com>
2018-10-31 18:05:53 -0300

    ldskjf

--- repoFunction.js
+++ repoFunction.js
@@ -10,8 +10,8 @@ var methods = {
 
 		var appKey = "0ANrY1kgphIThAA04S4FiEajGw3ub";
 		var token = "0ANrY1kgB33FfWZ3URadMJTgcfv";
-		var startDate = 2018-10-31T08:00:00;
-		var endDate = 2019-10-30T18:00:00;
+		var startDate = "2018-10-31T08:00:00";
+		var endDate = "2019-10-30T18:00:00";
 
 		var url =
 		"https://app.auvo.com.br/api/v1.0/tasks?appKey="+ appKey +"&token="+ token +
