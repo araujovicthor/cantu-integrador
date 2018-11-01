8820b8bce8765a420644f2bc12136b98cdc5ba24
Erlan <erlan.neo@gmail.com>
2018-11-01 19:29:31 -0300

    checks de ordem

---
+++ app/models/person.js
@@ -0,0 +1,21 @@
+/**
+ * Arquivo: produto.js
+ * Author: Glaucia Lemos
+ * Descrição: arquivo responsável onde trataremos o modelo da classe 'Produto'
+ * Data: 18/10/2017
+ * obs.: http://mongoosejs.com/docs/schematypes.html
+ */
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var PersonSchema = new Schema({
+    personName: String,
+    personPhone: String,
+    personEmail: String,
+    imovelURL: String,
+    taskStatus: String,
+    taskID: Number
+});
+
+module.exports = mongoose.model('Person', PersonSchema);
